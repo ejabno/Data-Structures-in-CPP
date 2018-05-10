07fe28f8ca4006c70e09a89e6efe3f6a789e5071
ejabno <jbak123@live.com>
2018-05-10 09:13:13 -0600

    Initial Commit

---
+++ Data Structures/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Data Structures
+//
+//  Created by Erickson Ureta on 2018-05-10.
+//  Copyright © 2018 Erickson Ureta. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
